e74ba8a00ce01ca568b1109939e5f0e0cffd2193
Eric Garcia <egarcia1868@gmail.com>
2025-03-06 13:55:22 -0500

    Fetching Data

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,48 @@
-const Home = () => {
-  return <div className="home">
-    <h2>Home</h2>
-  </div>
-}
+import { useEffect, useState } from "react";
 
-export default Home
+import WorkoutDetails from '../components/WorkoutDetails';
+
+const Home = () => {
+  const [workouts, setWorkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+
+      // const params = new URLSearchParams({
+      //   artistName: 'Billy Strings',
+      //   date: '14-12-2024'
+      // });
+      // const headers = {
+      //   Accept: "application/json",
+      //   "x-api-key": process.env.SETLIST_FM_API_KEY
+      // };
+    
+      // try {
+          // const response = await fetch(`https://api.setlist.fm/rest/1.0/search/setlists/${params}`, {
+          // const response = await fetch(`https://api.setlist.fm/rest/1.0/search/setlist/63de4613`, {
+          //   method: 'GET',
+          //   headers
+          // });
+
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkouts(json);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => <WorkoutDetails key={workout._id} workout={workout}/>)
+        }
+      </div>
+    </div>
+  );
+};
+
+export default Home;
