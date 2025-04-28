3cf5cc31141296428e72307e6e54fef3f78de493
Eric Garcia <egarcia1868@gmail.com>
2025-04-28 18:31:08 -0400

    further setup cookies

--- backend/server.js
+++ backend/server.js
@@ -5,18 +5,20 @@ import path from "path";
 import cors from "cors";
 import concertRoutes from "./routes/concertRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
+import cookieParser from 'cookie-parser';
 
 dotenv.config({ path: path.resolve("backend", ".env") });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const nodeEnv = process.env.NODE_ENV;
 
 const corsOptions = {
   methods: ["GET", "POST", "DELETE", "UPDATE"],
   credentials: true,
 };
 
-if (process.env.NODE_ENV === "production") {
+if (nodeEnv === "production") {
   // Allow only the frontend URL when in production
   corsOptions.origin = "https://set-trackr.onrender.com";
 } else {
@@ -25,12 +27,29 @@ if (process.env.NODE_ENV === "production") {
 }
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/concerts", concertRoutes);
 app.use("/api/auth", authRoutes);
 
 // Add a simple test route for backend
 app.get("/", (req, res) => {
   res.send("Backend is running!");
+});
+
+app.get('/set-cookies', (req, res) => {
+  // res.setHeader('Set-Cookie', 'newUser=true');
+  res.cookie('newUser', false);
+  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: nodeEnv === 'production' ? true : false });
+
+  res.send('you got the cookies!');
+
+})
+
+app.get('/read-cookies', (req, res) => {
+  const cookies = req.cookies;
+
+  console.log('cookies: ', cookies.newUser);
+  res.json(cookies);
 });
 
 //connect to db
