17f6c2db83ec430ba31b34e2ac2948c51f1bad1c
Eric Garcia <egarcia1868@gmail.com>
2025-06-25 15:03:50 -0500

    adding modal close on outside click

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
 import "./index.css";
 import App from "./App";
 import { ConcertsContextProvider } from "./context/ConcertsContext";
 
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <ConcertsContextProvider>
-      <App />
-    </ConcertsContextProvider>
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{ redirect_uri:window.location.origin }}
+    >
+      <ConcertsContextProvider>
+        <App />
+      </ConcertsContextProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import { useConcertsContext } from "../hooks/useConcertsContext";
 import { BASE_URL } from "../utils/config";
+import { useAuth0 } from "@auth0/auth0-react";
 
 import UnderConstructionModal from "../components/UnderConstructionModal";
 import ArtistDetails from "../components/ArtistDetails";
 import ConcertForm from "../components/ConcertForm";
 
 const Home = () => {
+  const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
   const { artists, dispatch } = useConcertsContext();
   const [isModalOpen, setIsModalOpen] = useState(true);
 
@@ -33,6 +35,8 @@ const Home = () => {
         onClose={() => setIsModalOpen(false)}
         isOpen={isModalOpen}
       />
+      {isAuthenticated ? (
+        <>
       <div className="concerts">
         {!artists && <h3>No Saved Concerts yet</h3>}
         {artists &&
@@ -41,7 +45,18 @@ const Home = () => {
           ))}
       </div>
       <ConcertForm />
+      </>
+        ) : (
+        <div className="auth-buttons">
+          <h2>Welcome to Set Trackr!</h2>
+          <p>Please log in to view and manage your concerts.</p>
+          <button onClick={() => loginWithRedirect()}>Log In</button>
+          <button onClick={() => logout({ returnTo: window.location.origin })}>
+            Log Out
+          </button>
     </div>
+        )}
+        </div>    
   );
 };
 
