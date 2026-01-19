83d85948d798d66680df0c5311a7fb187fb82f42
Eric Garcia <egarcia1868@gmail.com>
2026-01-19 15:17:10 -0600

    removing underConstructionModal

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,27 +1,29 @@
-import { useState } from "react";
-
-import UnderConstructionModal from "../components/common/UnderConstructionModal";
-
 const Home = () => {
-  const [isModalOpen, setIsModalOpen] = useState(true);
-
   return (
     <div className="home">
-      <UnderConstructionModal
-        onClose={() => setIsModalOpen(false)}
-        isOpen={isModalOpen}
-      />
-    <div>
-      <p>
-        Set Track'r is a tool that can be used to track the concerts and songs
-        performed at shows you attended.
-      </p>
-      <p>Please log in to view and manage your concerts.</p>
-      <p className="subtitle">
-        (if you do not wish to setup a login and just want to test this app use
-        demo@gmail.com & Demo!234)
-      </p>
-    </div>
+      <div className="home-content">
+        <h1>Welcome to Set Track'r</h1>
+        <p>
+          Set Track'r is a tool that can be used to track the concerts and songs
+          performed at shows you attended.
+        </p>
+        <p>Please log in to view and manage your concerts.</p>
+
+        <div className="roadmap-section">
+          <h3>Coming Soon</h3>
+          <ul>
+            <li>Search for users who have seen a specific artist or attended the same show</li>
+            <li>Search conversations by keyword</li>
+            <li>Block users from viewing your profile</li>
+            <li>Block users from sending messages</li>
+          </ul>
+        </div>
+
+        <p className="subtitle">
+          (if you do not wish to create an account and just want to test this app use
+          demo@gmail.com & Demo!234 to login to my demo account)
+        </p>
+      </div>
     </div>
   );
 };
